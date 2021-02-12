8599972e7ed2d4acf00b29ac37c5c6e432c255cd
brendon cecilio <brendon.cecilio@gmail.com>
2021-02-12 00:38:40 -0500

    bar button item presents colorPickerVC

---
+++ ColorPickerApp/Controllers/ColorPickerController.swift
@@ -0,0 +1,28 @@
+//
+//  ColorPickerController.swift
+//  ColorPickerApp
+//
+//  Created by Brendon Cecilio on 2/12/21.
+//
+
+import UIKit
+
+class ColorPickerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ColorPickerApp/Controllers/ViewController.swift
+++ ColorPickerApp/Controllers/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Colors"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: Selector("action"))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(presentColorPickerVC(_:)))
         view = mainView
     }
 
+    @objc private func presentColorPickerVC(_ sender: UIBarButtonItem) {
+        self.present(ColorPickerController(), animated: true)
+    }
 
 }
 

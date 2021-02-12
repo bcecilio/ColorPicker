5023c6436dffc99062c77e46547c1f6d3cdd81e7
brendon cecilio <brendon.cecilio@gmail.com>
2021-02-12 00:03:54 -0500

    finally starting color picker app

---
+++ ColorPickerApp/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ColorPickerApp
+//
+//  Created by Brendon Cecilio on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let mainView = MainView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = mainView
+    }
+
+
+}
+
---
+++ ColorPickerApp/Views/MainView.swift
@@ -0,0 +1,43 @@
+//
+//  MainView.swift
+//  ColorPickerApp
+//
+//  Created by Brendon Cecilio on 2/11/21.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    public var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collection.backgroundColor = .orange
+        return collection
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
